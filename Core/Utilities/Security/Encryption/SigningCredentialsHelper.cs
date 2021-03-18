dee8597348a038b1531e998b5c0ba00b647fda56
Goksal TAK <goksaltak@hotmail.com>
2021-03-19 00:01:43 +0300

    190320210001-2

---
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Encryption
+{
+    class SigningCredentialsHelper
+    {
+
+        public static SigningCredentials CreateSingningCredentials(SecurityKey securityKey)
+        {
+            return new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+        }
+    }
+}
