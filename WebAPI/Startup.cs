205129e13f170bdd94b3cfa49485f688ad3e7d4b
Goksal TAK <goksaltak@hotmail.com>
2021-04-04 00:23:26 +0300

    04042021

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -40,6 +40,9 @@ namespace WebAPI
             services.AddControllers();
             //services.AddSingleton<IProductService,ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
+            services.AddCors();
+
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -67,6 +70,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
